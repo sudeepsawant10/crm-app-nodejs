d4c562c1407591d46dc598c0eac6f2a349fff37e
sudeepsawant10 <sudeepsawant3710@gmail.com>
2023-07-02 10:41:49 +0530

    server requests using expressjs

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import routes from "./src/routes/crmRoutes"
+
+const app = express()
+const PORT = 3000
+
+routes(app);
+
+// ENDPOINTS 
+app.get('/', (req, res) => 
+    res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () => 
+    console.log(`Your server is running on port ${PORT}`)
+);
